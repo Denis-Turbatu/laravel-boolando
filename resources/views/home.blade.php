ab0364308a23defa092ac38610ead39b907310cc
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-07 15:02:03 +0200

    feat: html tags and data in header.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@include('./partials/header')
 
 @section('content')
     <h1>prova home</h1>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <ul>
+        <li>Uomo</li>
+        <li>Donne</li>
+        <li>Bambini</li>
+    </ul>
+
+    <div>
+        <img src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="Logo Boolean">
+    </div>
+    <ul>
+        <li>USER</li>
+        <li>HEART</li>
+        <li>SHOP</li>
+    </ul>
+</header>
