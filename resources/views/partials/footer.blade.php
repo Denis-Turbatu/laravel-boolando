68f2aeff2a4c22ff4d6651fbd6609e635b5197b2
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-07 16:32:38 +0200

    feat: ajustments style for main, header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,23 +1,22 @@
 <footer>
-
-    <div>
-        <h6>Boolean S.r.l.</h6>
-        <ul>
-            <li>Informazioni legali</li>
-            <li>Normativa sulla privacy</li>
-            <li>Diritto di recesso</li>
-        </ul>
-    </div>
-
-
-    <div>
-        <h6>Trovaci anche su</h6>
-        <ul>
-            <li>TWITTER</li>
-            <li>FACEBOOK</li>
-            <li>INSTAGRAM</li>
-            <li>PINTEREST</li>
-            <li>YOUTUBE</li>
-        </ul>
+    <div class="container">
+        <div>
+            <h6>Boolean S.r.l.</h6>
+            <ul>
+                <li>Informazioni legali</li>
+                <li>Normativa sulla privacy</li>
+                <li>Diritto di recesso</li>
+            </ul>
+        </div>
+        <div>
+            <h6>Trovaci anche su</h6>
+            <ul>
+                <li>TWITTER</li>
+                <li>FACEBOOK</li>
+                <li>INSTAGRAM</li>
+                <li>PINTEREST</li>
+                <li>YOUTUBE</li>
+            </ul>
+        </div>
     </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,15 @@
 <header>
-    <ul>
-        <li>Uomo</li>
-        <li>Donne</li>
-        <li>Bambini</li>
-    </ul>
-
-    <img class="logo" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="Logo Boolean">
-
-    <ul>
-        <li>USER</li>
-        <li>HEART</li>
-        <li>SHOP</li>
-    </ul>
+    <div class="container">
+        <ul>
+            <li>Uomo</li>
+            <li>Donne</li>
+            <li>Bambini</li>
+        </ul>
+        <img class="logo" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="Logo Boolean">
+        <ul>
+            <li>USER</li>
+            <li>HEART</li>
+            <li>SHOP</li>
+        </ul>
+    </div>
 </header>
