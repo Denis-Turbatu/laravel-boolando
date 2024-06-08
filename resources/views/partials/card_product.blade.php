17c9d890b868fadabd562be65a4bb69f15340bcf
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-08 20:57:55 +0200

    feat: integrated money format at prices

--- resources/views/partials/card_product.blade.php
+++ resources/views/partials/card_product.blade.php
@@ -7,12 +7,12 @@
     @if (isset($item['badges']))
       @foreach ($item['badges'] as $badge)
         @if ($badge['type'] === 'discount')
-          <span class="card-text my-0 text-decoration-line-through">{{ $item['price'] }}</span>
-          <span class="card-text my-0 text-danger">{{ number_format($item['price'] - round($item['price'] * $badge['value'] / 100, 2), 2) }}</span>
+          <span class="card-text my-0 text-decoration-line-through">{{ $item['price'] }}€</span>
+          <span class="card-text my-0 text-danger">{{ number_format($item['price'] - round($item['price'] * $badge['value'] / 100, 2), 2)}}€</span>
           @break
         @endif
       @endforeach
-      @if (!isset($badge) || $badge['type'] !== 'discount')  <span class="card-text my-0 text-danger">{{ $item['price'] }}</span>
+      @if (!isset($badge) || $badge['type'] !== 'discount')  <span class="card-text my-0 text-danger">{{ $item['price'] }}€</span>
       @endif
     @endif
 </div>
