0f3dc420424d1f5d2cf425e77d6b2e042295ab06
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-09 09:22:08 +0200

    feat: add _card_product.scss + implemented retro card in card_product.blade.php + managed hver effect

--- resources/views/partials/card_product.blade.php
+++ resources/views/partials/card_product.blade.php
@@ -1,19 +1,32 @@
-<div class="card border border-0 rounded-0 my-4">
-    <img src="{{ Vite::asset('resources/img/' . $item['frontImage']) }}" class="card-img-top" alt="{{ $item['name'] }}">
+<div class="ms-card card border border-0 rounded-0 my-4">
+    <div class="img-container">
+        {{-- IMMAGINE FONTALE --}}
+        <img src="{{ Vite::asset('resources/img/' . $item['frontImage']) }}"
+            class="card-img-top ms-front-img position-relative top-0 start-0" alt="{{ $item['name'] }}">
+        {{-- IMMAGINE DIETRO --}}
+        <img src="{{ Vite::asset('resources/img/' . $item['backImage']) }}"
+            class="card-img-top ms-retro-img position-absolute top-0 start-0 " alt="{{ $item['name'] }}">
+    </div>
     <div class="card-body">
+        {{-- NOME BRAND --}}
         <h5 class="card-text fw-semibold my-0">{{ $item['brand'] }}</h5>
+        {{-- NOME ARTICOLO --}}
         <p class="card-text my-0">{{ $item['name'] }}</p>
 
-    @if (isset($item['badges']))
-      @foreach ($item['badges'] as $badge)
-        @if ($badge['type'] === 'discount')
-          <span class="card-text my-0 text-decoration-line-through">{{ $item['price'] }}€</span>
-          <span class="card-text my-0 text-danger">{{ number_format($item['price'] - round($item['price'] * $badge['value'] / 100, 2), 2)}}€</span>
-          @break
+        {{-- CONTROLLO ESISTENZA BADGES NELL'ARTICOLO --}}
+        @if (isset($item['badges']))
+            @foreach ($item['badges'] as $badge)
+                {{-- CONTROLLO ESISTENZA DISCOUNT NEL BADGE E STAMPA CORRETTA --}}
+                @if ($badge['type'] === 'discount')
+                    <span class="card-text my-0 text-decoration-line-through">{{ $item['price'] }}€</span>
+                    <span
+                        class="card-text my-0 text-danger">{{ number_format($item['price'] - round(($item['price'] * $badge['value']) / 100, 2), 2) }}€</span>
+                @break
+            @endif
+        @endforeach
+        @if (!isset($badge) || $badge['type'] !== 'discount')
+            <span class="card-text my-0 text-danger">{{ $item['price'] }}€</span>
         @endif
-      @endforeach
-      @if (!isset($badge) || $badge['type'] !== 'discount')  <span class="card-text my-0 text-danger">{{ $item['price'] }}€</span>
-      @endif
     @endif
 </div>
 </div>
