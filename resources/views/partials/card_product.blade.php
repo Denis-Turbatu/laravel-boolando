9b35cc4e0143a86f7c8b53856cbc702d1676f32f
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-09 09:51:09 +0200

    feat: implemented tags with correct display data + style based on tag's value

--- resources/views/partials/card_product.blade.php
+++ resources/views/partials/card_product.blade.php
@@ -1,11 +1,25 @@
 <div class="ms-card card border border-0 rounded-0 my-4">
-    <div class="img-container">
+    <div class="img-container position-relative">
         {{-- IMMAGINE FONTALE --}}
         <img src="{{ Vite::asset('resources/img/' . $item['frontImage']) }}"
             class="card-img-top ms-front-img position-relative top-0 start-0" alt="{{ $item['name'] }}">
         {{-- IMMAGINE DIETRO --}}
         <img src="{{ Vite::asset('resources/img/' . $item['backImage']) }}"
             class="card-img-top ms-retro-img position-absolute top-0 start-0 " alt="{{ $item['name'] }}">
+        <div class="ms-tag-container">
+            {{-- CONTROLLO ESISTENZA BADGES NELL'ARTICOLO --}}
+            @if (isset($item['badges']))
+                @foreach ($item['badges'] as $badge)
+                    {{-- CONTROLLO ESISTENZA TAG NEL BADGE E STAMPA CORRETTA --}}
+                    @if ($badge['type'] === 'tag')
+                        <span class="card-text my-0 p-2 bg-success text-white">{{ $badge['value'] }}</span>
+                    @endif
+                    @if ($badge['type'] === 'discount')
+                        <span class="card-text my-0 p-2 bg-danger text-white">-{{ $badge['value'] }}%</span>
+                    @endif
+                @endforeach
+            @endif
+        </div>
     </div>
     <div class="card-body">
         {{-- NOME BRAND --}}
